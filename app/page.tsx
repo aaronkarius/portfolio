07bd6dd96fde14a4de5a4d34897a15c2d8874c8f
slime5 <85314060+slime5@users.noreply.github.com>
2023-08-09 21:52:16 -0400

    removing unused import, and adding test

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ export default function HomePage() {
   return (
     <div>
       <AnimatedBackground />
-
       <DarkModeToggle />
+      <h1 className="relative z-10 flex flex-col items-center justify-center w-screen h-screen text-black dark:text-white">
+        uh.
+      </h1>
     </div>
   );
 }
--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { FaMoon, FaSun } from "react-icons/fa";
 
@@ -8,7 +7,7 @@ const DarkModeButton = () => {
 
   return (
     <button
-      className="absolute w-10 h-10 right-7 bottom-7"
+      className="absolute z-50 w-10 h-10 right-7 bottom-7"
       onClick={() => (theme === "dark" ? setTheme("light") : setTheme("dark"))}
     >
       {theme === "dark" ? <FaSun size={40} /> : <FaMoon size={40} />}
