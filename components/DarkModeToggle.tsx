02e2640c89aa3c925e79404809acad662fc3e48a
slime5 <85314060+slime5@users.noreply.github.com>
2023-08-20 19:52:24 -0400

    checking dark mode on launch

--- components/DarkModeToggle.tsx
+++ components/DarkModeToggle.tsx
@@ -1,9 +1,22 @@
 "use client";
 import { useTheme } from "next-themes";
+import { useEffect } from "react";
 import { FaMoon, FaSun } from "react-icons/fa";
 
 const DarkModeButton = () => {
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <button
